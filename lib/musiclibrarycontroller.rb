41d5c0fc218dc94f555df16d117b20afd76f0828
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-09-12 13:22:22 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -23,7 +23,7 @@ class MusicLibraryController
   
   def list_songs
     count = 1
-    Song.all.sort_by().each{|song|
+    Song.all.sort_by{|song| song.name}.each{|song|
     puts "#{count}. #{song.artist.name}  - #{song.name} - #{song.genre.name}"
     count += 1
     }
